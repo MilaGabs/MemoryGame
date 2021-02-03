c4a5f693ca4a59c06c07176187bc4197f9aadf06
Gabriel <gteixeira539@gmail.com>
2021-02-02 22:27:24 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Gabriel Teixeira on 02/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
